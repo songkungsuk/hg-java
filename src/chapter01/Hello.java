71162bbd4391d36dfc620fe2826023a368298cce
songkungsug <k.s.song@runtime.co.kr>
2025-02-24 09:27:06 +0900

    git init

---
+++ src/chapter01/Hello.java
@@ -0,0 +1,14 @@
+package chapter01;
+
+public class Hello {
+	/*
+	 * 작성자 송경석
+	 * 작성일 2025-02-04
+	 * */
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello Java");
+	} // end of main
+}
+//end of class
